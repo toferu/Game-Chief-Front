5a2322179deb92fde021bdf5e441453b19edb4c5
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-08 16:34:38 -0500

    added api url. testing deployed updates

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ import Edit from './components/Edit'
 import './App.css';
 
 const App = () => {
-  const [games, setGames] = useState([])
+  const [games, setGames] = useState({})
 
-  const getGames = () => {
-    axios.get('http://').then(res => setGames(res.data), 
+  const getGames = (res) => {
+    axios.get("https://api.rawg.io/api/games?key=0fce8e91c7fa4616b4870ed4e6bfdcd8&page=10")
+    .then(res => setGames(res), 
     (err) => console.log(err)).catch((error) => console.log(error))
   }
 
+  useEffect(() => {
+    getGames()
+  }, [])
 
   return (
 
     <div className="">
 
-      <h1>Game Chief</h1>
+      <h1>Game</h1>
 
       {games.map((game) => {
         return (
           <>
-          <Games games = {game}/>
+          <Games game = {games}/>
           </>
         )
       })}
