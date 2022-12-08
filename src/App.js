16cc5674fb1af1a60f831c088a610590b13402b0
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-07 21:38:05 -0500

    added content to component files. Wrote get fn for App file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-import games from './components/Games'
+
+import Games from './components/Games'
+import Add from './components/Add'
+import Edit from './components/Edit'
+
 import './App.css';
 
 const App = () => {
-  const [games, setGames] = useState('')
+  const [games, setGames] = useState([])
 
-  const getGames () => {
-    
+  const getGames = () => {
+    axios.get('http://').then(res => setGames(res.data), 
+    (err) => console.log(err)).catch((error) => console.log(error))
   }
 
 
   return (
+
     <div className="">
-      
+
+      <h1>Game Chief</h1>
+
+      {games.map((game) => {
+        return (
+          <>
+          <Games games = {game}/>
+          </>
+        )
+      })}
+
     </div>
   );
 }
 
 export default App;
+
+//not sure about line 28. Unclear if it's games = {game} or game = {games}
