dc9eebb5c8bc00199aec0dfdd96c4fa1786fe47c
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-08 20:13:12 -0500

    created decisions component but it just has pseudo code now. moved experimental feature from it to app file. the feature returns a random genre from an array of genres.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,19 @@ import Edit from './components/Edit'
 import './App.css';
 
 const App = () => {
+
   const [games, setGames] = useState([])
+  // const [gameGenre, setgameGenre] = useState()
+  const [genre, setGenre] = useState('')
+  
+
+
+  const allGenres = ['Action', 'Indie', 'Adventure', 'RPG', 'Strategy', 'Shooter', 'Casual', 'Simulation', 'Puzzle', 'Arcade', 'Platformer', 'Racing', 'Family', 'Board Games', 'Educational', 'Card']
+  
+  const randomGenre = () => {
+    setGenre(allGenres[Math.floor(Math.random() * allGenres.length)])
+ }
+
 
   const getGames = (res) => {
     axios.get("https://api.rawg.io/api/games?key=0fce8e91c7fa4616b4870ed4e6bfdcd8&page=10")
@@ -16,16 +28,23 @@ const App = () => {
     (err) => console.log(err)).catch((error) => console.log(error))
   }
 
+  // const filtering = () => {
+  //   games.genre.includes(genre) ? 
+  // }
+  
+
   useEffect(() => {
     getGames()
+    randomGenre()
   }, [])
+
 
   return (
 
     <div className="">
 
-      <h1>Game 3</h1>
-
+      <h1>Game Chief</h1>
+      <p>{genre}</p>
       {games.map((game) => {
         return (
           <>
@@ -34,11 +53,9 @@ const App = () => {
         )
       })}
 
-
     </div>
   );
 }
 
 export default App;
 
-//not sure about line 28. Unclear if it's games = {game} or game = {games}
---
+++ src/components/Decisions.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+// random genre/tag compared to random genre/tag
+
+// selected genre/tag spread in to array and becomes filter for next Response
+
+// run an arbitray amount of Times
+
+// pick one from array
+
+// display 
+// api {props.games.genre}.length ??
+
+//  <button>{propssdfsd.games.tags}</button>
+
+//  pick random number within array.length then go inside and pick a random genre 
+
+const allGenres = ['Action', 'Indie', 'Adventure', 'RPG', 'Strategy', 'Shooter', 'Casual', 'Simulation', 'Puzzle', 'Arcade', 'Platformer', 'Racing', 'Family', 'Board Games', 'Educational', 'Card']
+
+const randomGenre = (allGenres) => {
+   setGenre(allGenres[Math.floor(Math.random() * allGenres.length)])
+
+}
+
+const [genre, setGenre] = useState('')
+
+genre.map((genre) =>{
+    return (
+        <p>{genre}</p>
+    )
+})
+
+const newList = (randomGenre) => {
+    for (let i = 0; i < game.genre.length; i++) {
+        if (game.genre[i] == randomGenre) {
+            List.push(game)
+        }
+    }
+}
+const Decision = (props) => {
+    let looped = game.genre.map(genre) // gives us array of genres in the game
+    let newList = (games.filter((game) => {
+        return looped.child == randomGenre 
+    })) 
+}
+
+// what do we want from decision? 
