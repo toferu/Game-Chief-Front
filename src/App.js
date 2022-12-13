7824e42e7df72ecaae45e99d4b669c7b05f55a43
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-13 18:05:23 -0500

    added hide for search

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Games from './components/Games'
 import Add from './components/Add'
 import Picking from './components/Picking'
 import Edit from './components/Edit'
-import Decision from './components/Decisions'
-
+// import Decision from './components/Decisions'
+import Search from './components/Search'
 import './App.css';
 
 const App = () => {
@@ -28,6 +28,27 @@ const App = () => {
         setGames([...games, response.data])
      })
   }
+  const handleEdit = (data) => {
+    axios.put('http://localhost:3000/games/' + data._id, data)
+    .then((response) => {
+       console.log(response)
+       let newGames = (games.map((game) => {
+        return game._id !== data._id ? game : data
+       }))
+       setGames(newGames)
+    })
+ }
+ const handleDelete = (deletedGame) => {
+  axios.delete('http://localhost:3000/games/' + deletedGame._id)
+  .then((response) => {
+   let newGames = games.filter((game) => {
+     return game._id !== response._id
+    })
+   setGames(newGames)
+   getGames()
+  })
+}
+
 
   useEffect(() => {
     getGames()
@@ -36,18 +57,20 @@ const App = () => {
 
   return (
     <div className="">
-      <Add handleCreate = {handleCreate}/>
+      {/* <Add handleCreate = {handleCreate}/>
       {games.map((game) => {
         return (
           <>
             <Games game = {game}/>
           </>
         )
-      })}
+      })} */}
       <h1>Game Chief</h1>
       <button onClick={() => setDisplay(!display)}> Find your perfect game</button>
       {display ? <Picking/> : null}
-       
+      <h3> Or</h3>
+       <h4 onClick={() => setDisplay(!display)} >Create Your Own Recommendation List</h4>
+      {display ?  <Search /> : null}
     </div>
   );
 }
