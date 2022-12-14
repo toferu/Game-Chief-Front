3540b4f19b79235ddf11a20ce1c94fcd321f45af
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-14 08:47:14 -0500

    removed changes as they caused the deployment to be rejected

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -3,19 +3,7 @@ import React,{useState, useEffect} from 'react'
 const Edit = (props) => {
     const [games, setGames] = useState([...props.game])
 
-    const getEditable = (data) => {
-        axios.get('https://gamechief-back.herokuapp.com/games' + data._id, data)
-        .then((response) => {
-           console.log(response)
-        //    let newGames = (games.map((game) => {
-        //     return game._id !== data._id ? game : data
-        //    }))
-          //  setGames(newGames)
-        })
-     }
 
-
-    }
     const handleChange = (event) => {
         setGames({...games, [event.target.name]: event.target.value})
     }
@@ -25,9 +13,6 @@ const Edit = (props) => {
         props.handleEdit(games)
     }
 
-useEffect(() => {
-    getEditable()
-}, [])
 
     return(
         <>
