cad8891994bd9ef1aaba54da647b21f69714a3ef
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-14 03:21:53 -0500

    I can't believe I have to deploy every time

--- src/components/Games.js
+++ src/components/Games.js
@@ -3,27 +3,32 @@ import {useState, useEffect} from 'react'
 
 
 const Games = (props) => {
-  // const [games, setGames] = useState(props)
+  const [games, setGames] = useState(...props)
   const [show, setShow] = useState(false)
-console.log(props)
+console.log(games.list)
+
+  // const funkyArray = () => {
+  //   setGames(games.list)
+  // }
   const showToggle = () => {
     setShow(true)
   }
   
-  return(
-     <>
-    <h3 onClick={() => showToggle}>{props.name}</h3>
-     {/* {props.map((li)=> {
-      return (
-        <> */}
-       <p>{props.name}</p>
-       <img src={props.background_image} alt="game cover"></img>
-       <p>Metacritic: {props.metacritic}</p>
+//   return(
+//      <>
+//     <h3 onClick={() => showToggle}>{games.name}</h3>
+//      {props.map((li)=> {
+//       return (
+//         <>
+//        <p>{props.name}</p>
+//        <img src={props.background_image} alt="game cover"></img>
+//        <p>Metacritic: {props.metacritic}</p>
        
-       {/* </> 
-    )
-  })} */}
-    </>
- )}
+//        </> 
+//     )
+//   })}
+//     </>
+//  )}
+}
 
  export default Games
