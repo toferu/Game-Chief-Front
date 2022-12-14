73f9b1b14d633712939fc10c0f3ba86809cd0a2b
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-14 03:05:53 -0500

    another attempt at passing the contents of props

--- src/components/Games.js
+++ src/components/Games.js
@@ -3,27 +3,27 @@ import {useState, useEffect} from 'react'
 
 
 const Games = (props) => {
-  const [games, setGames] = useState(props.list)
+  // const [games, setGames] = useState(props)
   const [show, setShow] = useState(false)
-console.log(games)
+console.log(props)
   const showToggle = () => {
     setShow(true)
   }
   
-//   return(
-//      <>
-//     <h3 onClick={() => showToggle}>{games.name}</h3>
-//      {games.list.map((li)=> {
-//       return (
-//         <>
-//        <p>{li.name}</p>
-//        <img src={li.background_image} alt="game cover"></img>
-//        <p>Metacritic: {li.metacritic}</p>
+  return(
+     <>
+    <h3 onClick={() => showToggle}>{props.name}</h3>
+     {props.map((li)=> {
+      return (
+        <>
+       <p>{li.name}</p>
+       <img src={li.list.background_image} alt="game cover"></img>
+       <p>Metacritic: {li.metacritic}</p>
        
-//        </> 
-//     )
-//   })}
-//     </>
-//  )}
-}
+       </> 
+    )
+  })}
+    </>
+ )}
+
  export default Games
