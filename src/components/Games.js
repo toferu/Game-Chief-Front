bc983c8df904cf3aef9519c3927001d98d1962d8
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-14 02:27:58 -0500

    commented return out and added console log to figure out what i'm working with

--- src/components/Games.js
+++ src/components/Games.js
@@ -5,25 +5,25 @@ import {useState, useEffect} from 'react'
 const Games = (props) => {
   const [games, setGames] = useState({...props.games})
   const [show, setShow] = useState(false)
-
+console.log(games)
   const showToggle = () => {
     setShow(true)
   }
   
-  return(
-     <>
-    <h3 onClick={() => showToggle}>{games.name}</h3>
-     {games.list.map((li)=> {
-      return (
-        <>
-       <p>{li.name}</p>
-       <img src={li.background_image} alt="game cover"></img>
-       <p>Metacritic: {li.metacritic}</p>
-       {/* <p>Platform: {props.game.platform}</p> */}
-       </> 
-    )
-  })}
-    </>
- )}
- 
+//   return(
+//      <>
+//     <h3 onClick={() => showToggle}>{games.name}</h3>
+//      {games.list.map((li)=> {
+//       return (
+//         <>
+//        <p>{li.name}</p>
+//        <img src={li.background_image} alt="game cover"></img>
+//        <p>Metacritic: {li.metacritic}</p>
+       
+//        </> 
+//     )
+//   })}
+//     </>
+//  )}
+}
  export default Games
