8ac966ad15f0287794380be260d7d60efed82cc3
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-14 02:21:12 -0500

    another attempt

--- src/components/Games.js
+++ src/components/Games.js
@@ -3,7 +3,7 @@ import {useState, useEffect} from 'react'
 
 
 const Games = (props) => {
-  // const [games, setGames] = useState({...props.games})
+  const [games, setGames] = useState({...props.games})
   const [show, setShow] = useState(false)
 
   const showToggle = () => {
@@ -12,13 +12,13 @@ const Games = (props) => {
   
   return(
      <>
-    <h3 onClick={() => showToggle}>{props.name}</h3>
-     {props.map((li)=> {
+    <h3 onClick={() => showToggle}>{games.name}</h3>
+     {games.list.map((li)=> {
       return (
         <>
-       <p>{li.list.name}</p>
-       <img src={li.list.background_image} alt="game cover"></img>
-       <p>Metacritic: {li.list.metacritic}</p>
+       <p>{li.name}</p>
+       <img src={li.background_image} alt="game cover"></img>
+       <p>Metacritic: {li.metacritic}</p>
        {/* <p>Platform: {props.game.platform}</p> */}
        </> 
     )
