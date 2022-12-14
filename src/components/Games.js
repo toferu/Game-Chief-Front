8a4f71c5060f413cafbaf61a07cce742cd174ebe
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-14 04:48:54 -0500

    troubleshooting props again for the header name

--- src/components/Games.js
+++ src/components/Games.js
@@ -3,7 +3,7 @@ import {useState, useEffect} from 'react'
 
 
 const Games = (props) => {
-  const [listName, setListName] = useState({...props.name})
+  const [listName, setListName] = useState([...props.name])
   const [games, setGames] = useState([...props.game.list])
   const [show, setShow] = useState(false)
 console.log(games)
